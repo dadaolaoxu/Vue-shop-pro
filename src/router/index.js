ef3cbe16e116a4ee25e1130362d355de6b35a1f0
xuruohang <872706750@qq.com>
2019-02-20 18:13:18 +0800

    后台首页开发完毕

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,40 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
-import Home from '@/components/Home'
+import Home from '@/components/Home' // 是App的子组件
+import Welcome from '@/components/Welcome' // 具体是home的子组件
+import User from '@/components/User' // 具体是home的子组件
 
 Vue.use(Router)
 
-export default new Router({
+var router = new Router({
   routes: [
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    { path: '/', redirect: '/home' },
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: User }
+      ]
+    }
   ]
 })
+// 路由导航守卫，检测token如果不存在，就跳转到login登录组件去
+router.beforeEach((to, from, next) => {
+  // 访问/login 就直接pass
+  if (to.path === '/login') {
+    return next()
+  }
+
+  // 访问非login，判断token
+  var token = window.sessionStorage.getItem('token')
+  if (!token) {
+    return next('/login')
+  }
+  next() // token存在，继续....
+})
+
+export default router
